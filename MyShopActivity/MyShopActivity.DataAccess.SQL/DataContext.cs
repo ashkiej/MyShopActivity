c8ece80784b3d24eb0c53bed761e842904b8f36b
Ashkie Edep <53522052+ashkiej@users.noreply.github.com>
2020-09-02 22:31:26 +0800

    Implemented SQL Repository

--- MyShopActivity/MyShopActivity.DataAccess.SQL/DataContext.cs
+++ MyShopActivity/MyShopActivity.DataAccess.SQL/DataContext.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace MyShopActivity.DataAccess.SQL
 {
-    class DataContext : DbContext
+    public class DataContext : DbContext
     {
         public DataContext()
             : base("DefaultConnection")
--- MyShopActivity/MyShopActivity.WebUI/App_Start/UnityConfig.cs
+++ MyShopActivity/MyShopActivity.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using MyShopActivity.Core.Contracts;
 using MyShopActivity.Core.Models;
 using MyShopActivity.DataAccess.InMemory;
+using MyShopActivity.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace MyShopActivity.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
